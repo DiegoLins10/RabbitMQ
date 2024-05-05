fcf3d70e82d15f370b2a546024ee3fa0a870c77b
diegolins10 <dede140126>
2024-05-05 00:49:33 -0300

    Criação de modulo de infra

---
+++ shipping-orders/src/DevTrackR.ShippingOrders.Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,60 @@
+﻿using DevTrackR.ShippingOrders.Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTrackR.ShippingOrders.Infrastructure
+{
+    public static class InfrastructureModule
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services
+                .AddMongo();
+
+            return services;
+        }
+
+        public static IServiceCollection AddMongo(this IServiceCollection services)
+        {
+            services.AddSingleton(sp =>
+            {
+                var configuration = sp.GetService<IConfiguration>();
+                var options = new MongoDbOptions();
+
+                configuration.GetSection("Mongo").Bind(options);
+
+                return options;
+            });
+
+            services.AddSingleton<IMongoClient>(sp =>
+            {
+                var configuration = sp.GetService<IConfiguration>();
+                var options = sp.GetService<MongoDbOptions>();
+
+                return new MongoClient(options.ConnectionString);
+            });
+
+            services.AddTransient(
+                sp =>
+            {
+                BsonDefaults.GuidRepresentation = GuidRepresentation.Standard;
+
+                var options = sp.GetService<MongoDbOptions>();
+                var mongoClient = sp.GetService<IMongoClient>();
+
+                var db = mongoClient.GetDatabase(options.Database);
+
+                return db;
+            });
+
+            return services;
+        }
+    }
+}
