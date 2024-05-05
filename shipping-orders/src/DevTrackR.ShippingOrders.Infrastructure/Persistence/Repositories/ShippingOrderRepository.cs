4e8f95f0209214bda3570cf79951a8b14494073d
diegolins10 <dede140126>
2024-05-05 00:50:00 -0300

    Criando repositories de acesso ao mongo

---
+++ shipping-orders/src/DevTrackR.ShippingOrders.Infrastructure/Persistence/Repositories/ShippingOrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using DevTrackR.ShippingOrders.Core.Entities;
+using DevTrackR.ShippingOrders.Core.Repositories;
+using MongoDB.Driver;
+
+namespace DevTrackR.ShippingOrders.Infrastructure.Persistence.Repositories
+{
+    public class ShippingOrderRepository : IShippingOrderRepository
+    {
+
+        private readonly IMongoCollection<ShippingOrder> _collection;
+
+        public ShippingOrderRepository(IMongoDatabase database)
+        {
+            _collection = database.GetCollection<ShippingOrder>("shipping-orders");
+        }
+
+        public Task AddAsync(ShippingOrder shippingOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShippingOrder> GetByCodeAsync(string code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
